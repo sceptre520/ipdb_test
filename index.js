95b748e6613bd7e959ea6fdab60b3fb6e63b521d
sceptre520 <sceptre520@gmail.com>
2021-11-13 05:30:49 +0200

    ss

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
-const driver = require('bigchaindb-driver')
+const BigchainDB = require('bigchaindb-driver')
 
-const alice = new driver.Ed25519Keypair()
-const conn = new driver.Connection('https://test.ipdb.io/api/v1/')
-const tx = driver.Transaction.makeCreateTransaction(
-    { message: 'Blockchain all the things!' },
-    null,
-    [ driver.Transaction.makeOutput(
-        driver.Transaction.makeEd25519Condition(alice.publicKey))],
-    alice.publicKey)
-const txSigned = driver.Transaction.signTransaction(tx, alice.privateKey)
-conn.postTransactionCommit(txSigned)
+const API_PATH = 'https://test.ipdb.io/api/v1/'
+const conn = new BigchainDB.Connection(API_PATH)
+
+const bip39 = require('bip39')
+
+const alice = new BigchainDB.Ed25519Keypair()
+
+const painting = {
+    name: 'Meninas',
+    author: 'Diego Rodríguez de Silva y Velázquez',
+    place: 'Madrid',
+    year: '1656'
+}
+
+function createPaint() {
+    // Construct a transaction payload
+    const txCreatePaint = BigchainDB.Transaction.makeCreateTransaction(
+        // Asset field
+        {
+            painting,
+        },
+        // Metadata field, contains information about the transaction itself
+        // (can be `null` if not needed)
+        {
+            datetime: new Date().toString(),
+            location: 'Madrid',
+            value: {
+                value_eur: '25000000€',
+                value_btc: '2200',
+            }
+        },
+        // Output. For this case we create a simple Ed25519 condition
+        [BigchainDB.Transaction.makeOutput(
+            BigchainDB.Transaction.makeEd25519Condition(alice.publicKey))],
+        // Issuers
+        alice.publicKey
+    )
+    // The owner of the painting signs the transaction
+    const txSigned = BigchainDB.Transaction.signTransaction(txCreatePaint,
+        alice.privateKey)
+
+    // Send the transaction off to BigchainDB
+    conn.postTransactionCommit(txSigned)
+        .then(res => {
+            // document.body.innerHTML += '<h3>Transaction created</h3>';
+            // document.body.innerHTML += txSigned.id
+            console.log(txSigned.id)
+            // txSigned.id corresponds to the asset id of the painting
+        })
+}
+
+createPaint()
